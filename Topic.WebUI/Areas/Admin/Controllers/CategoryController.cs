8ee7ff3762ee42d7c23c631c647dd85d5fcc7ac3
Sinantosun <sinan_tosun4141@hotmail.com>
2024-06-14 22:21:18 +0300

    --

---
+++ Topic.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using Topic.WebUI.Dtos.CategoryDtos;
+
+namespace Topic.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class CategoryController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var httpResponseMessage = await _httpClient.GetAsync("https://localhost:7074/api/Categories");
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await httpResponseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(result);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+
+            StringContent str = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PostAsync("https://localhost:7074/api/Categories", str);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var responseMessage = await _httpClient.DeleteAsync($"https://localhost:7074/api/Categories/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("awfawfawfaw");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var httpResponseMessage = await _httpClient.GetAsync($"https://localhost:7074/api/Categories/{id}");
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await httpResponseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(result);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto model)
+        {
+            var jsonData = JsonConvert.SerializeObject(model);
+
+            StringContent str = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await _httpClient.PutAsync("https://localhost:7074/api/Categories", str);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Topic.WebUI/Areas/Admin/Controllers/LayoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Topic.WebUI.Areas.Admin.Controllers
+{
+    public class LayoutController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
